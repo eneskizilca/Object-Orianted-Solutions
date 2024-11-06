af0864173565b6eddda80259fc95c1e29481bcce
Enes Kızılca <76004060+eneskizilca@users.noreply.github.com>
2024-11-06 17:50:41 +0300

    Add files via upload

---
+++ SorularVeCozumleri4/2.Soru/2.Soru/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Soru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Urun macbookPro = new Urun("Macbook Pro M3", 74000);
+                macbookPro.Indirim = 80;
+                decimal indirimliFiyat = macbookPro.IndirimliFiyat();
+                Console.WriteLine($"Eski fiyat: {macbookPro.Fiyat}, Yeni fiyat: {indirimliFiyat}");
+            } catch(ArgumentException)
+            {
+                Console.WriteLine("Indirim 0-50 arası olmalıdır.");
+            }
+            
+            Console.Read();
+        }
+
+        class Urun
+        {
+            private decimal indirim;
+            public string Ad { get; set; }
+            public decimal Fiyat { get; set; }
+            public decimal Indirim
+            {
+
+                get { return indirim; }
+                set
+                {
+                    if (value >= 0 && value <= 50)
+                        indirim = value;
+                    else
+                        throw new ArgumentException();
+                }
+            }
+
+            public Urun(string ad, decimal fiyat)
+            {
+                Ad = ad;
+                Fiyat = fiyat;
+            }
+
+            public decimal IndirimliFiyat()
+            {
+                return Fiyat - Fiyat * Indirim / 100;
+            }
+        }
+    }
+}
---
+++ SorularVeCozumleri4/5.Soru/5.Soru/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Soru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kutuphane kutuphane = new Kutuphane();
+
+            Kitap kitap1 = new Kitap("Savaş ve Barış", "Lev Tolstoy", 1869);
+            Kitap kitap2 = new Kitap("1984", "George Orwell", 1949);
+
+            kutuphane.KitapEkle(kitap1);
+            kutuphane.KitapEkle(kitap2);
+
+            kutuphane.KitaplariListele();
+            Console.ReadLine();
+        }
+    }
+
+    class Kitap
+    {
+        public string Baslik { get; set; }
+        public string Yazar { get; set; }
+        public int YayinYili { get; set; }
+
+        public Kitap(string baslik, string yazar, int yayinYili)
+        {
+            Baslik = baslik;
+            Yazar = yazar;
+            YayinYili = yayinYili;
+        }
+        public override string ToString()
+        {
+            return $"{Baslik} - {Yazar} ({YayinYili})";
+        }
+    }
+    class Kutuphane
+    {
+        public List<Kitap> kitaplar;
+
+        public Kutuphane()
+        {
+            kitaplar = new List<Kitap>();
+        }
+        public void KitapEkle(Kitap yeniKitap)
+        {
+            kitaplar.Add(yeniKitap);
+            Console.WriteLine($"{yeniKitap.Baslik} adlı kitap kütüphaneye eklendi.");
+        }
+        public void KitaplariListele()
+        {
+            Console.WriteLine("Kütüphanedeki Kitaplar:");
+            foreach (Kitap kitap in this.kitaplar)
+            {
+                Console.WriteLine(kitap);
+            }
+        }
+
+    }
+}
